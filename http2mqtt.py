7149b29f62c97d6821ded40bcdb2b391b5a0b3ae
Troels Agergaard Jacobsen <troels@ajonline.dk>
2019-09-30 20:57:55 +0200

    Whitelist allowed input; client_id as config param

--- http2mqtt.py
+++ http2mqtt.py
@@ -5,8 +5,10 @@ import paho.mqtt.client as mqtt
 import configparser
 
 class HTTP2MQTT(object):
-    def __init__(self, mqtt_client):
+    def __init__(self, mqtt_client, allowed_topics, allowed_payloads):
         self.mqtt_client = mqtt_client
+        self.allowed_topics = allowed_topics
+        self.allowed_payloads = allowed_payloads
 
     @cherrypy.expose
     def index(self):
@@ -14,8 +16,16 @@ class HTTP2MQTT(object):
 
     @cherrypy.expose
     def http2mqtt(self, topic=None, payload=""):
-        print("Topic: {}".format(topic))
-        print("Payload: {}".format(payload))
+        if topic in self.allowed_topics:
+            print("Topic: {}".format(topic))
+        else:
+            return "Invalid topic"
+
+        #if payload.isalnum():
+        if payload in self.allowed_payloads:
+            print("Payload: {}".format(payload))
+        else:
+            return "Invalid payload"
         
         self.mqtt_client.publish(topic, payload=payload)
 
@@ -25,17 +35,27 @@ if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read("config.ini")
 
-    username = config['mqtt-server']['username']
-    password = config['mqtt-server']['password']
-    host     = config['mqtt-server']['host']
-    port     = int(config['mqtt-server']['port'])
-    ca_certs = config['mqtt-server']['ca_certs']
+    username  = config['mqtt-server']['username']
+    password  = config['mqtt-server']['password']
+    host      = config['mqtt-server']['host']
+    port      = int(config['mqtt-server']['port'])
+    client_id = config['mqtt-server']['client_id']
+    ca_certs  = config['mqtt-server']['ca_certs']
 
-    mqtt_client = mqtt.Client(client_id="http2mqtt")
+    allowed_topics = config['http']['allowed_topics'].split(';')
+    allowed_payloads = config['http']['allowed_payloads'].split(';')
+
+    mqtt_client = mqtt.Client(client_id=client_id)
     mqtt_client.username_pw_set(username, password=password)
     mqtt_client.tls_set(ca_certs=ca_certs)
     mqtt_client.connect(host, port, 60)
 
-    cherrypy.quickstart(HTTP2MQTT(mqtt_client))
+    cherrypy.quickstart(
+            HTTP2MQTT(
+                mqtt_client,
+                allowed_topics,
+                allowed_payloads
+                )
+            )
 
     mqtt_client.disconnect()
